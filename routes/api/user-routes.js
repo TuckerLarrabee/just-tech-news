23b1700d42ae25eae77729123ce15159332aaabc
tuckl <tuckerlarrabee13@gmail.com>
2022-03-04 11:19:42 -0500

    validate pass

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -49,6 +49,28 @@ router.post('/', (req, res) => {
         })
 })
 
+router.post('/login', (req,res) => {
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    }).then(dbUserData => {
+        if (!dbUserData) {
+            res.status(400).json({message: 'No user with that email address!'});
+            return;
+        }
+        //verify user
+        const validPassword = dbUserData.checkPassword(req.body.password);
+        if (!validPassword){
+            res.status(400).json({message: 'incorrect Password!'});
+        }
+
+        res.json({ user: dbUserData, message: "youre now logged in!"})
+
+        
+    })
+})
+
 //PUT /api/users/1
 router.put('/:id', (req, res) => {
     //expects {username: 'sdafadsf', email: 'adsfdf@gmail.com, password: 'sadfasfd'}
