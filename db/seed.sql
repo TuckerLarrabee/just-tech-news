645c161a526be2367dbfe82713f957f3978a54a1
tuckl <tuckerlarrabee13@gmail.com>
2022-03-04 15:28:36 -0500

    Post work completed

---
+++ db/seed.sql
@@ -0,0 +1,3 @@
+USE just_tech_news_db
+INSERT INTO post (title, post_url, user_id, created_at, updated_at)
+VALUES ("Taskmaster goes public!", "https://taskmaster/press", 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
