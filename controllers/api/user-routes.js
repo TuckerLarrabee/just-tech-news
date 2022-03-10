c0e08b554cc63cceee01722476b23d518137bf58
tuckl <tuckerlarrabee13@gmail.com>
2022-03-10 11:32:14 -0500

    logout

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -53,6 +53,7 @@ router.get('/:id', (req, res) => {
     });
 });
 
+//sign up user/create user
 router.post('/', (req, res) => {
   // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
   User.create({
@@ -60,17 +61,23 @@ router.post('/', (req, res) => {
     email: req.body.email,
     password: req.body.password
   })
-    .then(dbUserData => {
+  .then(dbUserData => {
+    req.session.save(() => {
+      req.session.user_id = dbUserData.id;
+      req.session.username = dbUserData.username;
+      req.session.loggedIn = true;
+  
       res.json(dbUserData);
-    })
+    });
+  })
     .catch(err => {
       console.log(err);
       res.status(500).json(err);
     });
 });
 
+//login user
 router.post('/login', (req, res) => {
-  // expects {email: 'lernantino@gmail.com', password: 'password1234'}
   User.findOne({
     where: {
       email: req.body.email
@@ -88,7 +95,14 @@ router.post('/login', (req, res) => {
       return;
     }
 
-    res.json({ user: dbUserData, message: 'You are now logged in!' });
+    req.session.save(() => {
+      // declare session variables
+      req.session.user_id = dbUserData.id;
+      req.session.username = dbUserData.username;
+      req.session.loggedIn = true;
+
+      res.json({ user: dbUserData, message: 'You are now logged in!' });
+    });
   });
 });
 
@@ -113,6 +127,17 @@ router.put('/:id', (req, res) => {
       console.log(err);
       res.status(500).json(err);
     });
+});
+
+router.post('/logout', (req,res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    })
+  }
+  else {
+    res.status(404).end();
+  }
 });
 
 router.delete('/:id', (req, res) => {
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -5,6 +5,7 @@ const { Post, User, Comment} = require('../models');
 // get all posts for homepage
 router.get('/', (req, res) => {
   console.log('======================');
+  console.log(req.session)
   Post.findAll({
     attributes: [
       'id',
@@ -40,5 +41,13 @@ router.get('/', (req, res) => {
       res.status(500).json(err);
     });
 });
+
+router.get('/login', (req,res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }  
+  res.render('login')
+})
 
 module.exports = router;
